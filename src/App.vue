e9decfaaa92b375eb6dd2ff3ae2f91a47be3b622
sweet202020 <dolcealessio1@gmail.com>
2022-11-09 16:39:55 +0100

    add bonus

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,40 @@
 <!-- Create un nuovo progetto utilizzando Vite, aiutatevi con le slide per ripercorrere i vari passaggi dell'installazione come visti a lezione.
 Create e utilizzate un componente AppTitle, il quale contiene un titolo che recita "La mia prima app con Vite!" -->
 
+<!-- Bonus:
+Creare un secondo componente AppHero, che visualizza un jumbotron. -->
+
 
 
 
 
 <script>
 import AppTitle from "./components/AppTitle.vue";
+import AppHero from "./components/AppHero.vue"
 
 export default {
   components: {
     AppTitle,
+    AppHero
 
-  }
+  },
+  /* data() {
+    return {
+      image: 'jumbo.jpg'
+    }
+  },
+  methods: {
+    getImagePath: function (imgPath) {
+      return new URL(imgPath, import.meta.url).href
+    }
+  } */
 }
 </script>
 
 
 <template>
   <AppTitle />
+  <AppHero />
 </template>
 
 
---
+++ src/components/AppHero.vue
@@ -0,0 +1,11 @@
+
+<script>
+
+</script>
+
+
+<template>
+    <div class="jumbo">
+        <img src="../assets/jumbo.jpg" alt="">
+    </div>
+</template>
