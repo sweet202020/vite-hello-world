27138ee0431ade259c9198aa677d7cd1e5327150
sweet202020 <dolcealessio1@gmail.com>
2022-11-09 15:50:57 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<!-- Create un nuovo progetto utilizzando Vite, aiutatevi con le slide per ripercorrere i vari passaggi dell'installazione come visti a lezione.
+Create e utilizzate un componente AppTitle, il quale contiene un titolo che recita "La mia prima app con Vite!" -->
+
+
+
+
+
+<script>
+import AppTitle from "./components/AppTitle.vue";
+
+export default {
+  components: {
+    AppTitle,
+
+  }
+}
+</script>
+
+
+<template>
+  <AppTitle />
+</template>
+
+
+
+
+<style>
+body {
+  background-color: yellow;
+}
+</style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,3 @@
+<template>
+    <h1>La mia prima app con Vite!</h1>
+</template>
